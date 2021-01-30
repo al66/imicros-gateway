e0091a1308f534a775d65c164f2602ef802a590b
al66 <andreas.leinen@imicros.de>
2021-01-30 16:40:07 +0100

    added login with service token / call to agents

--- test/gateway.spec.js
+++ test/gateway.spec.js
@@ -5,13 +5,17 @@ const { Gateway } = require("../index");
 const _ = require("lodash");
 const request = require("supertest");
 const jwt 	= require("jsonwebtoken");
+const fs = require("fs");
+const { Constants } = require("../lib/util/constants");
+
+const JWT_SECRET = "my_super_jwt_secret_for_users_service";
 
 const GatewaySettings = {
     name: "gateway",
     version: 1,
     settings: {
-        authorization: {
-            service: "v1.users"
+        services: {
+            users: "v1.users"
         },
         routes: [
             {
@@ -76,18 +80,6 @@ const GatewaySettings = {
                     // File upload from AJAX or cURL
                     "PUT /:objectName": "stream:v1.minio.putObject",
 
-                    // File upload from HTML form and overwrite busboy config
-                    "POST /multi": {
-                        type: "multipart",
-                        // Action level busboy config
-                        busboyConfig: {
-                            limits: {
-                                files: 3
-                            }
-                        },
-                        action: "v1.minio.putObject"
-                    },
-                    
                     "GET /:objectName": "v1.minio.getObject",
                     
                     "GET /stat/:objectName": "v1.minio.statObject",
@@ -101,15 +93,8 @@ const GatewaySettings = {
                     
                     _.set(ctx, "meta.filename",_.get(req,"$params.objectName",req.headers["x-imicros-filename"]));
                     _.set(ctx, "meta.mimetype",req.headers["x-imicros-mimetype"]);
-                },            
-            
-                // https://github.com/mscdex/busboy#busboy-methods
-                busboyConfig: {
-                    limits: {
-                        files: 1
-                    },
-                    onFilesLimit: jest.fn()
-                },
+                }
+                
             }
         ]
     }
@@ -135,7 +120,8 @@ const Users = {
                 return {
                     action: "login",
                     params: ctx.params,
-                    meta: ctx.meta
+                    meta: ctx.meta,
+                    token: jwt.sign({ type: "user_token", id: "any", valid: true },JWT_SECRET)
                 };
             }
         },
@@ -159,6 +145,27 @@ const Users = {
                             id: "xyz", 
                             email: "test@test.de",
                             verified: true
+                        }
+                    };
+                }
+                throw new Error("Unvalid token");
+            }
+        }
+    }
+};
+
+// mock agents service
+const Agents = {
+    name: "agents",
+    actions: {
+        verify:  {
+            handler(ctx) {
+                let decoded = jwt.decode(ctx.params.token);
+                if (decoded.valid) {
+                    return { 
+                        service: {
+                            token: ctx.params.token,
+                            id: "xyz"
                         }
                     };
                 }
@@ -197,6 +204,24 @@ const Files = {
                     meta: ctx.meta
                 };
             }
+        },
+        putObject: {
+            async handler(ctx) {
+                let fstream = fs.createWriteStream("assets/imicros.received.png");
+                function receive(stream) {
+                    return new Promise(resolve => {
+                        stream.pipe(fstream);
+                        fstream.on("close", () => {
+                            resolve();
+                        });
+                    });
+                } 
+                await receive(ctx.params);
+                return {
+                    action: "putObject",
+                    meta: ctx.meta
+                };
+            }
         }
     }
         
@@ -215,14 +240,10 @@ describe("Test Gateway", () => {
                 logLevel: "info" //"debug"
             });
             gatewayService = await broker.createService(Gateway,GatewaySettings);
-            users = await broker.createService(Users);
-            service = await broker.createService(Service);
-            files = await broker.createService(Files);
+            // load additonal services
+            [Agents, Users, Service, Files].map(service => { return broker.createService(service); }); 
             await broker.start();
             server = gatewayService.server;
-            expect(users).toBeDefined();
-            expect(service).toBeDefined();
-            expect(files).toBeDefined();
             expect(gatewayService).toBeDefined();
         });
 
@@ -283,7 +304,7 @@ describe("Test Gateway", () => {
         });
 
         it("it should return user", () => {
-            let token = jwt.sign({ type: "identity", valid: true }, "mySecret");
+            let token = jwt.sign({ type: Constants.USER_TOKEN , valid: true }, "mySecret");
             return request(server)
                 .get("/users/me")
                 .set("Authorization","Bearer "+token)
@@ -309,7 +330,7 @@ describe("Test Gateway", () => {
         });
         
         it("it should return status 401 - missing authorization", () => {
-            let token = jwt.sign({ type: "identity", valid: false }, "mySecret");
+            let token = jwt.sign({ type: Constants.USER_TOKEN, valid: false }, "mySecret");
             return request(server)
                 .get("/users/me")
                 .set("Authorization","Bearer "+token)
@@ -328,7 +349,7 @@ describe("Test Gateway", () => {
     describe("Test path files", () => {
 
         it("it should call files service", () => {
-            let token = jwt.sign({ type: "identity", valid: true }, "mySecret");
+            let token = jwt.sign({ type: Constants.USER_TOKEN, valid: true }, "mySecret");
             return request(server)
                 .get("/files/test")
                 .set("Authorization","Bearer "+token)
@@ -336,6 +357,20 @@ describe("Test Gateway", () => {
                     expect(res.statusCode).toBe(200);
                     expect(res.body).toBeDefined();
                     expect(res.body.params.objectName).toEqual("test");
+                });
+        });
+
+        it("it should upload the attached files", () => {
+            let token = jwt.sign({ type: Constants.USER_TOKEN, valid: true }, "mySecret");
+            return request(server)
+                .post("/files")
+                .set("Authorization","Bearer "+token)
+                .attach("imicros_1.png","assets/imicros.png")
+                .attach("imicros_2.png","assets/imicros.png")
+                .then(res => {
+                    expect(res.statusCode).toBe(200);
+                    expect(res.body).toEqual(expect.arrayContaining([expect.objectContaining({ meta: expect.objectContaining({ fieldname: "imicros_1.png" })})]));
+                    expect(res.body).toEqual(expect.arrayContaining([expect.objectContaining({ meta: expect.objectContaining({ fieldname: "imicros_2.png" })})]));
                 });
         });
         
@@ -357,7 +392,7 @@ describe("Test Gateway", () => {
         });
 
         it("it should call service", () => {
-            let token = jwt.sign({ type: "identity", valid: true }, "mySecret");
+            let token = jwt.sign({ type: Constants.USER_TOKEN, valid: true }, "mySecret");
             let accessToken = "xyz";
             return request(server)
                 .get("/v1/example/action")
@@ -387,7 +422,7 @@ describe("Test Gateway", () => {
         });
         
         it("it should return status 401 - missing authorization", () => {
-            let token = jwt.sign({ type: "identity", valid: false }, "mySecret");
+            let token = jwt.sign({ type: Constants.USER_TOKEN, valid: false }, "mySecret");
             return request(server)
                 .get("/users/me")
                 .set("Authorization","Bearer "+token)
@@ -414,6 +449,21 @@ describe("Test Gateway", () => {
                     expect(res.body.message).toEqual("Unauthorized");
                     expect(res.body.code).toEqual(401);
                     expect(res.body.type).toEqual("INVALID_TOKEN");
+                });
+        });
+
+        it("it should call service with service token", () => {
+            let token = jwt.sign({ type: Constants.SERVICE_TOKEN, valid: true }, "mySecret");
+            let accessToken = "xyz";
+            return request(server)
+                .get("/v1/example/action")
+                .set("Authorization","Bearer "+token)
+                .set("x-imicros-xtoken",accessToken)
+                .then(res => {
+                    expect(res.statusCode).toBe(200);
+                    expect(res.body).toBeDefined();
+                    expect(res.body.action).toEqual("action");
+                    expect(res.body.meta.acl.accessToken).toEqual(accessToken);
                 });
         });
         
